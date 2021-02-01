25e22a9bc6f396e512ea3d7e1493caf51d93cb1a
Glen Adamson <glen.adamson@relative.uk.com>
2021-02-01 13:41:29 +0000

    adding register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,24 +3,47 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
+        @foreach($errors->all() as $error)
+            {{ $error  }}
+        @endforeach
+
         <div class="form-group">
             <label>Name</label>
-            <input name="name" value="{{ old('name') }}" required class="form-control" />
+            <input name="name" value="{{ old('name') }}" required class="form-control"/>
+
+            @if($errors->has('name'))
+            <span class="invalid-feedback">
+                <strong>{{ $errors->first('name') }}</strong>
+            </span>
+            @endif
+
         </div>
 
         <div class="form-group">
             <label>Email</label>
-            <input name="email" value="{{ old('email') }}" required class="form-control" />
+            <input name="email" value="{{ old('email') }}" required class="form-control"/>
+            @if($errors->has('email'))
+                <span class="invalid-feedback">
+                <strong>{{ $errors->first('email') }}</strong>
+            </span>
+            @endif
         </div>
 
         <div class="form-group">
             <label>Password</label>
-            <input name="password" required class="form-control" />
+            <label>
+                <input name="password" required class="form-control" type="password" {{ $errors->has('password') ? ' is-invalid': ''}}/>
+            </label>
+            @if($errors->has('password'))
+                <span class="invalid-feedback">
+                <strong>{{ $errors->first('password') }}</strong>
+            </span>
+            @endif
         </div>
 
         <div class="form-group">
             <label>Retyped Password</label>
-            <input name="password_confirmation" required class="form-control" />
+            <input name="password_confirmation" required class="form-control" type="password" />
         </div>
 
         <button type="submit" class="btn btn-primary btn-block">Register</button>
