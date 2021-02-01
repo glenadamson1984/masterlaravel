43e46f9087cdc7b8a9b24c8f55355cf01ae14246
Glen Adamson <glen.adamson@relative.uk.com>
2021-02-01 20:56:41 +0000

    error messages

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,43 +3,31 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        @foreach($errors->all() as $error)
-            {{ $error  }}
-        @endforeach
-
         <div class="form-group">
             <label>Name</label>
             <input name="name" value="{{ old('name') }}" required class="form-control"/>
-
-            @if($errors->has('name'))
-            <span class="invalid-feedback">
-                <strong>{{ $errors->first('name') }}</strong>
-            </span>
-            @endif
-
         </div>
+        @if($errors->has('name'))
+            <label>{{$errors->first('name')}}</label>
+        @endif
 
         <div class="form-group">
             <label>Email</label>
             <input name="email" value="{{ old('email') }}" required class="form-control"/>
-            @if($errors->has('email'))
-                <span class="invalid-feedback">
-                <strong>{{ $errors->first('email') }}</strong>
-            </span>
-            @endif
         </div>
+        @if($errors->has('email'))
+            <label>{{$errors->first('email')}}</label>
+        @endif
 
         <div class="form-group">
             <label>Password</label>
             <label>
                 <input name="password" required class="form-control" type="password" {{ $errors->has('password') ? ' is-invalid': ''}}/>
             </label>
-            @if($errors->has('password'))
-                <span class="invalid-feedback">
-                <strong>{{ $errors->first('password') }}</strong>
-            </span>
-            @endif
         </div>
+        @if($errors->has('password'))
+            <label>{{$errors->first('password')}}</label>
+        @endif
 
         <div class="form-group">
             <label>Retyped Password</label>
